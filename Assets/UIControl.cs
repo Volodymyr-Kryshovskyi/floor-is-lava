1db933108a9a4d43ed1be33438874ba9eaf2477b
Volodymyr-Kryshovskyi <trplovich6@gmail.com>
2025-11-15 19:55:00 +0100

    Сделал контейнер коинов и уиконтроль для них

--- Assets/UIControl.cs
+++ Assets/UIControl.cs
@@ -1,14 +1,14 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using TMPro;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
 
-// public class UIControl : MonoBehaviour
-// {
-//     [SerializeField] private TMP_Text coins;
+public class UIControl : MonoBehaviour
+{
+    [SerializeField] private TMP_Text coins;
 
-//     void Update()
-//     {
-//         coins.text = DataContainer.coins;
-//     }
-// }
+    void Update()
+    {
+        coins.text = DataContainer.coins.ToString();
+    }
+}
